2645402aaa96345b3bba18de6b149487b8f54e5d
Parijat Barua <141837424+pgithub03@users.noreply.github.com>
2024-07-25 20:09:36 +0000

    dataTypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,10 @@
+// int => 2^ 53
+//string => ""
+// boolean = true/false
+//null = standalone value
+//undefined =>
+// symbol => unique
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
