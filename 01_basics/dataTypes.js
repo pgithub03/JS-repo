bec152d8e5d6c3c45854190e0a8ed5e29880085d
Parijat Barua <141837424+pgithub03@users.noreply.github.com>
2024-07-26 16:08:02 +0000

    dataTypes

--- 01_basics/dataTypes.js
+++ 01_basics/dataTypes.js
@@ -6,8 +6,10 @@
 // symbol => unique
 // object
 
-console.log(typeof undefined); // undefined
-console.log(typeof null); // object
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
+
+//+++++++++++++++++++++++++++++++++++++++++++++
 
 //conversion output
 
@@ -20,5 +22,65 @@ console.log(typeof null); // object
 //1 => true ; 0 => false
 // "" => false
 // "abc" => true
+
+//console.log(1+2+"2") => 32
+//console.log("1"+2+2) => 122
+
+//+++++++++++++++++++++++++++++++++++++++++++++
+
+//Types of dataTypes 
+
+//1. Primitive(call by value)=> 7 types : String , Number , Boolean , null , undefined , Symbol:to make smtg unique
+
+//Js is dynamically typed lang
+
+
+//2. Reference/Non primitive(call by reference) => Array, Objects, Functions 
+
+// const id = Symbol("123")
+// const newid = Symbol("123") 
+// console.log(id === newid) => false
+
+// let obj={
+//     Name:"Parijat",
+//     age : 22
+// }
+ 
+// const myfunc=function(){
+//     console.log("Hello world");
+// }
+
+//+++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack(Primitve) => copy of declarations used , original value not changed
+// Heap(Non primitive) => reference to original variable => original value changed 
+
+//String
+
+// console.log(`Hwllo my name is ${"Parijat"} and salary is ${12000}`) //String interpolation
+
+// const gameName = new String('Parijat')
+
+// const  newName = gameName.substring(0,4)
+// console.log(newName);
+
+// console.log(newName.trim());
+
+const url = "https://snjtb%20"
+//  console.log(url.replace('%20','-'))
+console.log(url);
+
+// console.log(gameName.split('-'));
+
+// const balance = new Number(200)
+
+// console.log(balance.toFixed(2)) //=> 200.00
+// console.log(balance.toPrecision(2)) //=> 200.00
+// console.log(balance.toLocaleString('en-IN'))
+
+// Maths 
+
+console.log(Math); //=> object
+
 
 
