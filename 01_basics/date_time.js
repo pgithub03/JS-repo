ced7f245d663be5ea30417000f45a870abd460a6
Parijat Barua <141837424+pgithub03@users.noreply.github.com>
2024-07-26 16:36:47 +0000

    dataTypes

---
+++ 01_basics/date_time.js
@@ -0,0 +1,13 @@
+//Dates
+//date is a object 
+
+// let date = new Date()
+
+// console.log(date.toUTCString());
+
+let myDate = new Date("01-12-2024")
+console.log(myDate);
+
+myDate.toLocaleString('default',{
+    weekday:"long"
+})
