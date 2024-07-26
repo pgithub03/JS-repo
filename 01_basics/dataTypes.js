55349214c6fe41ede62b720af8d3795880767ba1
Parijat Barua <141837424+pgithub03@users.noreply.github.com>
2024-07-26 11:18:12 +0000

    datatypes

--- 01_basics/dataTypes.js
+++ 01_basics/dataTypes.js
@@ -8,3 +8,17 @@
 
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object
+
+//conversion output
+
+//To int
+// "33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
+
+//To Boolean
+//1 => true ; 0 => false
+// "" => false
+// "abc" => true
+
+
