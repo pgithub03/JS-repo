2f9c8b231d189ffb6b4886f7c814c25911f214db
Parijat Barua <141837424+pgithub03@users.noreply.github.com>
2024-07-27 15:06:26 +0000

    scope.js

--- 03_basics/scope.js
+++ 03_basics/scope.js
@@ -1,16 +1,45 @@
 //scope of windows browser and scope of code editor is different
 
-console.log(func1(2))// returns 3
-function func1(num){
-    return num + 1
-}
+// console.log(func1(2))// returns 3
+// function func1(num){
+//     return num + 1
+// }
 
-console.log(func2(2))// returns cannot find func2
-const func2 = function (num) {
-    return num + 1
+// console.log(func2(2))// returns cannot find func2
+// const func2 = function (num) {
+//     return num + 1
 
-}
+// }
 
-console.log(func2(2))// returns 3
+// console.log(func2(2))// returns 3
 
 
+//in browser global object is window
+
+// function chai(){
+//     console.log(this);
+// }
+
+// chai()
+
+// const chai = function(){
+//      console.log(this);
+// }
+
+// chai()
+
+// const chai = () => {
+//     console.log(this);
+// }
+
+// // chai()
+
+// const add = (n1 , n2) => { => explicit return 
+//     return n1+n2
+// }
+
+// const add = (n1 , n2) => n1+n2 //implicit return
+    
+const add = () => ({username : "parijat"})
+
+console.log(add());
