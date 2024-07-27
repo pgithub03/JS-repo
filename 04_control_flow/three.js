bdd7abe8acf83af5e150157253faf20a20d91b7a
Parijat Barua <141837424+pgithub03@users.noreply.github.com>
2024-07-27 20:11:12 +0000

    three.js

---
+++ 04_control_flow/three.js
@@ -0,0 +1,41 @@
+const code = ["js","py","cpp","java"]
+
+// code.forEach( function n(item){
+//     console.log(item);
+// })
+
+// code.forEach((item)=>{
+//     console.log(item);
+// })
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+//code.forEach(printMe)
+
+// code.forEach( (item, index , arr)=>{ 
+//     console.log(item,index,arr);
+// }) => returns js 0 [ 'js', 'py', 'cpp', 'java' ]
+//               py 1 [ 'js', 'py', 'cpp', 'java' ]
+//               cpp 2 [ 'js', 'py', 'cpp', 'java' ]
+//               java 3 [ 'js', 'py', 'cpp', 'java' ]
+
+const mycode = [
+    {
+        langname : "js",
+        langfile : ".js"
+    },
+    {
+        langname : "py",
+        langfile : ".py"
+    },
+    {
+        langname : "cpp",
+        langfile : ".cpp"
+    }
+]
+
+mycode.forEach((item)=>{
+    console.log(item.langfile);
+})
