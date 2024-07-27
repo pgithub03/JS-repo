6287e4d53921aaeb0a1c0fad915a4f11819b1dc7
Parijat Barua <141837424+pgithub03@users.noreply.github.com>
2024-07-27 13:59:55 +0000

    functions.js

---
+++ 03_basics/functions.js
@@ -0,0 +1,26 @@
+// FUNCTION... 
+
+// function func(){
+//     console.log("HELLO WORLD");
+// }
+
+// function add2no(n1 , n2){
+    
+//     return (n1 + n2);
+// }
+
+// const result = add2no(1,null);
+
+// console.log(result);
+
+// function login(username){
+//     return `${username} just loggied in`
+// }
+
+const arr = [200 ,300 ,400]
+
+function returnval(getArray){
+    return getArray[1]
+}
+
+console.log(returnval([100,100,2000]));
