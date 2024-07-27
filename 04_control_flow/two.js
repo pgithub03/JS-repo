f4f199588be303d15cd57f8c6ee8b202b3119543
Parijat Barua <141837424+pgithub03@users.noreply.github.com>
2024-07-27 20:11:21 +0000

    two.js

---
+++ 04_control_flow/two.js
@@ -0,0 +1,46 @@
+//array loops
+
+// const arr = [1,2,3,4]
+
+// for (const it of arr){
+//     console.log(it);
+// }
+
+// const name = "parijat"
+
+// for(const it of name){
+//     console.log(`${it}`);
+// }
+
+// //maps 
+
+// const map = new Map()
+// map.set('IN',"India")
+// map.set('GE',"Germany")
+
+// // // console.log(map);
+
+// for (const key in map) { //=> map is not iterable so doesnt run
+//       console.log(key);
+// }
+
+// for (const [key,value] of map) {
+//     console.log(key,'=',value);
+// }
+
+// const obj = {
+//     'name1' : 'harry',
+//     'name2' : 'parry',   
+//     'name3' : 'binod'
+// }
+
+// for (const key in obj) {
+//     console.log(`${obj[key]} is name for ${key}`);
+// }
+
+// const prog = ["js","ruby","cpp","py"]
+
+// for (const key in prog) {
+//    console.log(prog[key]);
+// }
+
