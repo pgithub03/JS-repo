20563a16f94c846a9e8782a7273f3cd8b0911a04
Parijat Barua <141837424+pgithub03@users.noreply.github.com>
2024-07-28 05:50:39 +0000

    filter_reduce.js

---
+++ 05_filter_reduce/filter_reduce.js
@@ -0,0 +1,36 @@
+// const books = [
+//     { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+//     { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+//     { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+//     { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+//     { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+//     { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+//     { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+//     { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+//     { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+//   ];
+
+// userBooks = books.filter((bk)=>{
+//     return bk.publish>=1995 && bk.genre==="History"
+// })
+
+// console.log(userBooks);
+
+const nums = [1,2,4,5,6,7,3]
+
+//const newnums = nums.map((num)=> {return num+10})
+//console.log(newnums);
+
+
+// chaining with maps 
+
+// const newnums = nums
+//             .map((num)=> num*10)
+//             .map((num)=>num+1)
+//             .filter((num)=>num>40)
+
+// console.log(newnums);
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
