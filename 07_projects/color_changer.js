c49c483974b31b97a5872fab0657912d4f3b2775
Parijat Barua <141837424+pgithub03@users.noreply.github.com>
2024-07-28 21:03:54 +0000

    color_changerjs

---
+++ 07_projects/color_changer.js
@@ -0,0 +1,26 @@
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector("body")
+
+
+buttons.forEach(function(btn){
+    console.log(btn);
+    btn.addEventListener('click',function(event){
+        console.log(event);
+        console.log(event.target);
+        if(event.target.id==='grey'){
+            body.style.backgroundColor = event.target.id
+        }
+        if(event.target.id==='white'){
+            body.style.backgroundColor = event.target.id
+        }
+        if(event.target.id==='blue'){
+            body.style.backgroundColor = event.target.id
+        }
+        if(event.target.id==='yellow'){
+            body.style.backgroundColor = event.target.id
+        }
+        if(event.target.id==='green'){
+            body.style.backgroundColor = event.target.id
+        }
+    })
+})
