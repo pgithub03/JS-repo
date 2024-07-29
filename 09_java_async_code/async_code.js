0333dbae9878c379bf2def89c73b99d22beaf26b
Parijat Barua <141837424+pgithub03@users.noreply.github.com>
2024-07-29 13:48:25 +0000

    async_code.js

---
+++ 09_java_async_code/async_code.js
@@ -0,0 +1,17 @@
+// Javascript is ------>
+
+//default js 
+// 1. Synchrnous 
+// 2. single threaded (js is slow but we  
+//                     do not use just js engine alone we use it with nodejs or smtg)
+
+
+//Execution Context ----> execute one line of code at a time 
+
+// Blocking Code Vs Non Blocking Code 
+
+// Blocking code -> Block the flow of the program --> Read File Sync
+
+// Non blocking code -> Do not block the flow of the program --> Read File Async
+
+
